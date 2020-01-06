75b0a585cf455b4dfbb4f3aae066ac7f036153ea
Leina33 <dominickrutto@gmail.com>
2020-01-06 09:45:12 +0300

    this are psql and time setting that will be used in db

--- instagram/views.py
+++ instagram/views.py
@@ -10,45 +10,24 @@ def welcome(request):
 #date function
 def instagram_of_day(request):
     date = dt.date.today()
-     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-def convert_dates(dates):
+    return render(request, 'display/home.html',{'date': date})
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 
 def past_days_instagram(request,past_date):
+    
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-        
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-        day = convert_dates(date)
-        html = f'''
-        <html>
-            <body>
-                <h1>Instagram page for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+        assert False
 
+    if date == dt.date.today():
+        return redirect(instagram_of_day)
+
+    return render(request, 'display/index.html', {"date":date})
     
 
 
