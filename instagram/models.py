ca2a846f80f63287b6efa1cd30bac028eb0bc070
Leina33 <dominickrutto@gmail.com>
2020-01-07 10:40:54 +0300

    import datetime module

--- instagram/models.py
+++ instagram/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 #importing user method for django
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField(default = 'default.jpg',upload_to = 'ProfilePicture/')
+    bio = models.TextField(max_length=200)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+        
+        
     
 class Image(models.Model):
     image = models.ImageField(upload_to='instagram/',null = 'False')
-    caption = models.CharField(max_length =500)
+    caption = models.CharField(max_length =60)
     
     def __str__(self):
         return self.caption
