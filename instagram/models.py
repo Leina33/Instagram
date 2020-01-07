5dbf24db273b995b2abe80f731be6acb501d2f9b
Leina33 <dominickrutto@gmail.com>
2020-01-07 09:17:51 +0300

    this are image variables that will represent columns in database instagram

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='instagram/',null = 'False')
+    caption = models.CharField(max_length =60)
+    
+    
