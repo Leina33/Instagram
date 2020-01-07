bdfee3a30c0f0e57a6386f9232e0142095a52794
Leina33 <dominickrutto@gmail.com>
2020-01-07 09:35:38 +0300

    update Image model, add __str__ that will return a string of representation of image models, this will be useful when i want to view return queries

--- instagram/models.py
+++ instagram/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to='instagram/',null = 'False')
-    caption = models.CharField(max_length =60)
+    caption = models.CharField(max_length =500)
     
-    
+    def __str__(self):
+        return self.caption
