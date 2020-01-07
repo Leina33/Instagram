baec101048f9cd0f96ba3c8f7f0e38e3c531daa9
Leina33 <dominickrutto@gmail.com>
2020-01-07 09:56:38 +0300

    create class profile that will be used to upload users pics

--- instagram/models.py
+++ instagram/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(default = 'default.jpg',upload_to = 'ProfilePicture/')
+    
 class Image(models.Model):
     image = models.ImageField(upload_to='instagram/',null = 'False')
     caption = models.CharField(max_length =500)
